eb3fd8d3da491ad3548a1cd5f5fbbf8ed1fc9cf1
Aashik1 <40072407+Aashik1@users.noreply.github.com>
2020-10-19 15:59:19 +0530

    uploading
     uploading numbers file

---
+++ Numbers.java
@@ -0,0 +1,18 @@
+package hcl;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		try {
+			int n = 1;
+			for (int i = 0; i <= 9; i++) {
+				System.out.println(n);
+				n++;
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
